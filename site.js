efec2ebe9bda6639e2a0c4ffb9176186aeb410a4
SILMAR CARDOSO DA SILVA JUNIOR <silmar_silva-junior@estudante.sesisenai.org.br>
2023-03-17 20:42:48 -0300

    Criar criado os arquivos html, js e css

---
+++ site.js
@@ -0,0 +1,9 @@
+
+
+    const sidebar = document.getElementById('sidebar');
+    const button = document.getElementById('btn');
+
+    button.addEventListener('click', function () {
+        sidebar.classList.toggle('fechar');
+    });
+
